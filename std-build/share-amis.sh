1376375bbc9fa29e20f9ca063cc8f07c205e085f
David Duncan <davdunc@amazon.com>
2020-11-08 20:11:15 -0800

    Update the AMI share script for standard partition

--- std-build/share-amis.sh
+++ std-build/share-amis.sh
@@ -1,23 +1,24 @@
-REGION=cn-northwest-1 
-snapshotId=${1:-snap-0ba13c36039a89f59}
-ImageId=${2:-ami-0217a0210ea0fec9c}
-	     
-
-aws ec2 modify-snapshot-attribute \
-    --snapshot-id $snapshotId \
-    --region $REGION \
-    --attribute createVolumePermission \
-    --operation-type add \
-    --user-ids 336777782633 882445023432 830553105645
-
-aws ec2 describe-snapshot-attribute --attribute createVolumePermission --snapshot-id $snapshotId
+#!/bin/bash
+set -x 
+REGION=us-east-1
+ImageId=$1
+snapshotId=$(aws ec2 describe-images --region $REGION --owners self --image-ids $ImageId --query 'Images[].BlockDeviceMappings[].Ebs.SnapshotId' --output text)
 
 aws ec2 modify-image-attribute \
     --image-id $ImageId  \
     --region $REGION \
     --attribute launchPermission \
     --operation-type add \
-    --user-ids 336777782633 882445023432 830553105645
+    --user-ids 679593333241 684062674729 425685993791 425685993791
 
-aws ec2 describe-image-attribute --attribute launchPermission --image-id $ImageId
+aws ec2 describe-snapshots --snapshot-ids $snapshotId --region $REGION && \
+aws ec2 modify-snapshot-attribute \
+    --snapshot-id $snapshotId \
+    --region $REGION \
+    --attribute createVolumePermission \
+    --operation-type add \
+    --user-ids 679593333241 684062674729 425685993791 425685993791
+
+aws ec2 describe-snapshot-attribute --region $REGION --attribute createVolumePermission --snapshot-id $snapshotId
+aws ec2 describe-image-attribute --region $REGION --attribute launchPermission --image-id $ImageId
 
