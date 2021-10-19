4b3735a08901dc9261eb377fe9d7f857c9e57309
David Duncan <davdunc@amazon.com>
2021-10-19 05:25:01 -0700

    Update the accounts to include mvp operations
    Signed-off-by: David Duncan <davdunc@amazon.com>

--- std-build/share-amis.sh
+++ std-build/share-amis.sh
@@ -9,7 +9,7 @@ aws ec2 modify-image-attribute \
     --region $REGION \
     --attribute launchPermission \
     --operation-type add \
-    --user-ids 679593333241 684062674729 425685993791 
+    --user-ids 679593333241 684062674729 425685993791 514427062609
 
 aws ec2 describe-snapshots --snapshot-ids $snapshotId --region $REGION && \
 aws ec2 modify-snapshot-attribute \
@@ -17,8 +17,12 @@ aws ec2 modify-snapshot-attribute \
     --region $REGION \
     --attribute createVolumePermission \
     --operation-type add \
-    --user-ids 679593333241 684062674729 425685993791
+    --user-ids 679593333241 684062674729 425685993791 514427062609
 
 aws ec2 describe-snapshot-attribute --region $REGION --attribute createVolumePermission --snapshot-id $snapshotId
 aws ec2 describe-image-attribute --region $REGION --attribute launchPermission --image-id $ImageId
 
+# 514427062609 is ec2-mvp-ops@amazon.com
+# 425685993791 is a test account for validations from Red Hat
+# 684062674729 is the account for sharing to the aws marketplace
+# 679593333241 is the account for testing in the aws marketplace
