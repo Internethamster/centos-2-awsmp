f1cab82c47109642f2d5f74742181f37dfaa8f1c
David Duncan <davdunc@amazon.com>
2022-03-15 01:09:47 -0400

    syntax error in conditional statements and housekeeping
    Signed-off-by: David Duncan <davdunc@amazon.com>

--- std-build/shared_functions.sh
+++ std-build/shared_functions.sh
@@ -12,48 +12,53 @@ exit_abnormal() {
 }
 function get_s3_bucket_location () {
     local BUCKET_NAME=$1
-    local STORAGE_REGION=$(aws s3api get-bucket-location --bucket $BUCKET_NAME --query 'LocationConstraint' --output text)
-    if [[ "$STORAGE_REGION" == "null"]] 
+    local STORAGE_REGION=$(aws s3api get-bucket-location \
+			       --bucket $BUCKET_NAME \
+			       --query 'LocationConstraint' \
+			       --output text
+	  )
+
+    if [[ "$STORAGE_REGION" == "null" ]]
     then
-    STORAGE_REGION="us-east-1"
+	STORAGE_REGION="us-east-1"
     fi
 }
 
 while getopts ":v:b:k:a:n:r:R:dp" options; do
     case "${options}" in
-        v)
-            VERSION=${OPTARG}
-            ;;
-        b)
-            S3_BUCKET=${OPTARG}
-            ;;
-        k)
-            S3_PREFIX=${OPTARG}
-            ;;
-        r)
-            RELEASE=${OPTARG}
-            ;;
-        R)
-            REGION=${OPTARG}
-            ;;
-        a)
-            ARCH=${OPTARG}
-            ;;
-        n)
-            NAME=${OPTARG}
-            ;;
-        d)
-            DRY_RUN="--dry-run"
-            ;;
-        p)
-            PSTATE="true"
-            ;;
-        :)
-            "Error: -${OPTARG} requires an argument"
-            ;;
-        *)
-            exit_abnormal
-            ;;
+	v)
+	    VERSION=${OPTARG}
+	    ;;
+	b)
+	    S3_BUCKET=${OPTARG}
+	    ;;
+	k)
+	    S3_PREFIX=${OPTARG}
+	    ;;
+	r)
+	    RELEASE=${OPTARG}
+	    ;;
+	R)
+	    REGION=${OPTARG}
+	    ;;
+	a)
+	    ARCH=${OPTARG}
+	    ;;
+	n)
+	    NAME=${OPTARG}
+	    ;;
+	d)
+	    DRY_RUN="--dry-run"
+	    ;;
+	p)
+	    PSTATE="true"
+	    ;;
+	:)
+	    "Error: -${OPTARG} requires an argument"
+	    ;;
+	*)
+	    exit_abnormal
+	    ;;
     esac
 done
 
@@ -71,12 +76,12 @@ get_default_sg_for_vpc () {
 }
 
 function copy_snapshot_to_us-east-1 {
-    
+
 if [[ "$REGION" != "us-east-1" ]]
 then
-    
+
     aws ec2 copy-snapshot --source-region $REGION --source-snapshot-id $snapshotId \
-        --destination-region us-east-1 \
-        --description "Import Base CentOS${MAJOR_RELEASE} ${ARCHITECTURE} Image"
+	--destination-region us-east-1 \
+	--description "Import Base CentOS${MAJOR_RELEASE} ${ARCHITECTURE} Image"
 fi
 }
