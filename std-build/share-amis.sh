7d372d4cba45a9f7fdfb2ad6a91c80fee2aef0ad
David Duncan <davdunc@amazon.com>
2022-03-22 19:04:20 -0700

    add: Include sharing the AMIs to the Lightsail team
    Signed-off-by: David Duncan <davdunc@amazon.com>

--- std-build/share-amis.sh
+++ std-build/share-amis.sh
@@ -9,7 +9,7 @@ aws ec2 modify-image-attribute \
     --region $REGION \
     --attribute launchPermission \
     --operation-type add \
-    --user-ids 679593333241 684062674729 425685993791 514427062609
+    --user-ids 679593333241 684062674729 425685993791 514427062609 014813956182
 
 aws ec2 describe-snapshots --snapshot-ids $snapshotId --region $REGION && \
 aws ec2 modify-snapshot-attribute \
@@ -17,7 +17,7 @@ aws ec2 modify-snapshot-attribute \
     --region $REGION \
     --attribute createVolumePermission \
     --operation-type add \
-    --user-ids 679593333241 684062674729 425685993791 514427062609
+    --user-ids 679593333241 684062674729 425685993791 514427062609 014813956182
 
 aws ec2 describe-snapshot-attribute --region $REGION --attribute createVolumePermission --snapshot-id $snapshotId
 aws ec2 describe-image-attribute --region $REGION --attribute launchPermission --image-id $ImageId
@@ -26,3 +26,4 @@ aws ec2 describe-image-attribute --region $REGION --attribute launchPermission -
 # 425685993791 is a test account for validations from Red Hat
 # 684062674729 is the account for sharing to the aws marketplace
 # 679593333241 is the account for testing in the aws marketplace
+# 014813956182 is the account for testing in the Lightsail group
