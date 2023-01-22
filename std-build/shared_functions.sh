da8c2e6e8427538003aa51b43dfc95b7cee6e1ed
David Duncan <davdunc@davidduncan.org>
2023-01-22 17:35:06 -0600

    fix: make room for the version information
     # The commit type can include the following:
    
     #: feat – a new feature is introduced with the changes
     #: fix – a bug fix has occurred
     #: chore – changes that do not relate to a fix or feature and don't modify src or test files (for example updating dependencies)
     #: refactor – refactored code that neither fixes a bug nor adds a feature
     #: docs – updates to documentation such as a the README or other markdown files
     #: style – changes that do not affect the meaning of the code, likely related to code formatting such as white-space, missing semi-colons, and so on.
     #: test – including new or correcting previous tests
     #: perf – performance improvements
     #: ci – continuous integration related
     #: build – changes that affect the build system or external dependencies
     #: revert – reverts a previous commit
    
    Signed-off-by: David Duncan <davdunc@davidduncan.org>

--- std-build/shared_functions.sh
+++ std-build/shared_functions.sh
@@ -1,6 +1,43 @@
 usage() {
     echo "Usage: $0 [ -v VERSION ] [ -b BUCKET_NAME ] [ -k OBJECT_PREFIX ] [ -a ARCH ] [ -n NAME ] [ -r RELEASE ] [ -R REGION ] [ -d DRY_RUN ]" 1>&2
 }
+VERSION=${VERSION:-FIXME}
+REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document  | jq -r ".region")
+
+function err() {
+    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+}
+
+exit_abnormal() {
+    usage
+    exit 1
+}
+
+function get_s3_bucket_location () {
+    local BUCKET_NAME=$1
+
+    local STORAGE_REGION=$(aws s3api get-bucket-location --bucket $BUCKET_NAME \
+		--query 'LocationConstraint' --output text)
+    if [[ "$STORAGE_REGION" == "null" ]]
+    then
+	STORAGE_REGION="us-east-1"
+    fi
+    printf "%s" $STORAGE_REGION
+}
+
+get_default_vpc_subnet () {
+    local REGION=${1:-$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document  | jq -r ".region")}
+    local VPC_ID=$(aws ec2 describe-vpcs --region $REGION --query "Vpcs[?IsDefault].VpcId" --output text)
+    aws ec2 describe-subnets --region $REGION --filters "Name=vpc-id,Values=$VPC_ID" --query "Subnets[?MapPublicIpOnLaunch] | [0].SubnetId" --output text
+
+}
+
+get_default_sg_for_vpc () {
+    local REGION=${1:-$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document  | jq -r ".region")}
+    local VPC_ID=$(aws ec2 describe-vpcs --region $REGION --query "Vpcs[?IsDefault].VpcId" --output text)
+    aws ec2 describe-security-groups --region $REGION --filters "Name=vpc-id,Values=${VPC_ID}" --query 'SecurityGroups[?GroupName == `default`].GroupId' --output text
+}
+
 while getopts ":f:v:b:k:a:n:r:R:dp" options; do
     case "${options}" in
 	v)
