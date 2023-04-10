082ce523d88b856e06c46b0df7383cb89c42a7ef
David Duncan <davdunc@amazon.com>
2023-04-09 23:46:29 -0700

    fix: make the ssm put parameter call variable
    Signed-off-by: David Duncan <davdunc@amazon.com>

--- cn-build/shared_functions.sh
+++ cn-build/shared_functions.sh
@@ -2,9 +2,9 @@ function put_ssm_parameters() {
     SSM_NAME=${DATE}-${VERSION}
     err "saving ssm public parameter /amis/centos/${ARCHITECTURE}/${NAME}/${SSM_NAME}"
     err "saving ssm public parameter /amis/centos/${ARCHITECTURE}/${NAME}/latest" 
-    aws ssm put-parameter --name "/amis/centos/${ARCHITECTURE}/centos-stream-ec2-8/${SSM_NAME}"  \
+    aws ssm put-parameter --name "/amis/centos/${ARCHITECTURE}/${NAME,,}/${SSM_NAME}"  \
         --type "String" --value $ImageId --data-type "aws:ec2:image"
-    aws ssm put-parameter --name "/amis/centos/${ARCHITECTURE}/centos-stream-ec2-8/latest"  \
+    aws ssm put-parameter --name "/amis/centos/${ARCHITECTURE}/${NAME,,}/latest"  \
         --type "String" --value $ImageId --data-type "aws:ec2:image"
 }
 
