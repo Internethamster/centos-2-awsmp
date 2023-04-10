c4e5831683606f695f2f58937ffaf908d5e6a4ce
David Duncan <davdunc@amazon.com>
2023-04-10 05:49:55 -0700

    feat: support for CentOS-Stream-9 in cn-build
    Signed-off-by: David Duncan <davdunc@amazon.com>

---
+++ cn-build/centos-stream-9-config.sh
@@ -0,0 +1,42 @@
+BASE_URI="https://cloud.centos.org/centos"
+S3_BUCKET="davdunc-floppy"
+S3_PREFIX="disk-images"
+
+MAJOR_RELEASE=9
+NAME="CentOS-Stream-ec2"
+ARCH=$(arch)
+
+if [[ "$ARCH" == "aarch64" ]]; then
+    ARCHITECTURE="arm64"
+    CPE_RELEASE=1
+    CPE_RELEASE_DATE=20230405
+    CPE_RELEASE_REVISION=
+    FILE_NAME_PREFIX="${NAME}-${ARCH}-${MAJOR_RELEASE}"
+
+    QEMU_IMG="taskset -c 1 qemu-img"
+    VIRT_CUSTOMIZE="taskset -c 1 virt-customize"
+    VIRT_EDIT="taskset -c 1 virt-edit"
+    VIRT_SYSPREP="taskset -c 1 virt-sysprep"
+
+    INSTANCE_TYPE="m6g.large"
+else
+    ARCHITECTURE="$(arch)"
+    CPE_RELEASE=1
+    CPE_RELEASE_DATE=20230405
+    CPE_RELEASE_REVISION=
+    FILE_NAME_PREFIX="${NAME}-${MAJOR_RELEASE}"
+
+    QEMU_IMG="qemu-img"
+    VIRT_CUSTOMIZE="virt-customize"
+    VIRT_EDIT="virt-edit"
+    VIRT_SYSPREP="virt-sysprep"
+
+    INSTANCE_TYPE="m6i.large"
+fi
+
+UPSTREAM_RELEASE="${MAJOR_RELEASE}-stream"
+MINOR_RELEASE="${CPE_RELEASE_DATE}.${CPE_RELEASE}"
+UPSTREAM_FILE_NAME="${FILE_NAME_PREFIX}-${MINOR_RELEASE}.${ARCH}"
+
+# ${Base_URI}/$UPSTREAM_RELEASE/${ARCH}/images/${FILE_NAME_PREFIX)-${UPSTREAM_FILE_NAME}.${ARCH}.raw.xz
+# ${BASE_URI}/$UPSTREAM_RELEASE/${ARCH}/images/${FILE_NAME_PREFIX}-${UPSTREAM_FILE_NAME}.${ARCH}.raw.xz
---
+++ cn-build/image-builder-9-stream.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+# CENTOS-9-STREAM BUILDER for CN
+set -euo pipefail
+
+source ${0%/*}/centos-stream-9-config.sh
+source ${0%/*}/shared_functions.sh
+# ${0%/*}/download-image.py
+
+DATE=$(date +%Y%m%d)
+
+NAME="CentOS-Stream-ec2"
+# GenericImage="https://cloud.centos.org/centos/8-stream/aarch64/images/CentOS-Stream-ec2-8-20210603.0.aarch64.qcow2"
+
+REGION=cn-northwest-1
+SUBNET_ID=subnet-0890b142
+SECURITY_GROUP_ID=sg-5993f530
+DRY_RUN="--dry-run"
+
+
+if [[ -z $REGION ]]
+then
+    exit_abnormal
+fi
+
+if [[ ! -e ${NAME}-${MAJOR_RELEASE}-${DATE}.txt ]]
+then
+    echo "0" > ${NAME}-${MAJOR_RELEASE}-${DATE}.txt
+fi
+
+LINK="${BASE_URI}/${UPSTREAM_RELEASE}/$ARCH/images/${UPSTREAM_FILE_NAME}.qcow2"
+LINK2="${BASE_URI}/${MAJOR_RELEASE}-stream/${ARCH}/images/${NAME}-${MAJOR_RELEASE}-${MINOR_RELEASE}.${ARCH}.qcow2"
+
+if [ ! -e ${NAME}-${MAJOR_RELEASE}-${DATE}.txt ]; then
+    echo "0" > ${NAME}-${MAJOR_RELEASE}-${DATE}.txt
+fi
+
+if [ "$VERSION" == "FIXME" ]; then
+    echo $(( $(cat ${NAME}-${MAJOR_RELEASE}-${DATE}.txt) + 1 )) > ${NAME}-${MAJOR_RELEASE}-${DATE}.txt
+    VERSION=$(cat ${NAME}-${MAJOR_RELEASE}-${DATE}.txt)
+fi
+
+IMAGE_NAME="${NAME}-${MAJOR_RELEASE}-${DATE}.${VERSION}.${ARCH}"
+
+curl -C - -o ${UPSTREAM_FILE_NAME}.qcow2 $LINK
+
+err "$LINK retrieved and saved at $(pwd)/${UPSTREAM_FILE_NAME}.qcow2"
+
+CMD="qemu-img convert"
+if [[ "$ARCHITECTURE" == "arm64" ]]; then
+    CMD="taskset -c 0 $CMD"
+fi
+
+$CMD ./${UPSTREAM_FILE_NAME}.qcow2 ${IMAGE_NAME}.raw
+err "${IMAGE_NAME}.raw created" 
+
+CMD="virt-edit"
+if [[ "$ARCHITECTURE" == "arm64" ]]; then
+    CMD="taskset -c 0  $CMD"
+fi
+$CMD ./${IMAGE_NAME}.raw /etc/sysconfig/selinux -e "s/^\(SELINUX=\).*/\1permissive/"
+err "Modified ./${IMAGE_NAME}.raw to make it permissive"
+
+CMD="virt-customize"
+if [[ "$ARCHITECTURE" == "arm64" ]]; then
+    CMD="taskset -c 0 $CMD"
+fi
+$CMD -a ./${IMAGE_NAME}.raw  --update --install cloud-init
+err "virt-customize -a ./${IMAGE_NAME}.raw  --update --install cloud-init"
+
+# virt-edit ./${IMAGE_NAME}.raw  /etc/cloud/cloud.cfg -e "s/name: centos/name: ec2-user/"
+# err "Modified Image to move centos to ec2-user"
+
+CMD=virt-customize
+if [[ "$ARCHITECTURE" == "arm64" ]]; then
+    CMD="taskset -c 0 $CMD"
+fi
+$CMD -a ./${IMAGE_NAME}.raw --selinux-relabel
+err "virt-customize -a ./${IMAGE_NAME}.raw --selinux relabel"
+
+CMD=virt-sysprep
+
+if [[ "$ARCHITECTURE" == "arm64" ]]
+then
+    CMD="taskset -c 0 $CMD"
+fi
+$CMD -a ./${IMAGE_NAME}.raw
+err "upgrading the current packages for the instance: ${IMAGE_NAME}"
+
+err "Cleaned up the volume in preparation for the AWS Marketplace"
+
+aws s3 cp ./${IMAGE_NAME}.raw  s3://${S3_BUCKET}/${S3_PREFIX}/
+err "Upload ${IMAGE_NAME}.raw image to S3://${S3_BUCKET}/${S3_PREFIX}/"
+
+DISK_CONTAINER="Description=\'${IMAGE_NAME}\',Format=raw,UserBucket={S3Bucket=${S3_BUCKET},S3Key=${S3_PREFIX}/${IMAGE_NAME}.raw}"
+
+err DISK_CONTAINER="$DISK_CONTAINER"
+IMPORT_SNAP=$(aws ec2 import-snapshot --region $REGION --client-token ${IMAGE_NAME}-$(date +%s) --description "Import Base $NAME ($ARCH) Image" --disk-container "$DISK_CONTAINER")
+err "snapshot suceessfully imported to $IMPORT_SNAP"
+
+snapshotTask=$(echo $IMPORT_SNAP | jq -Mr '.ImportTaskId')
+
+while [[ "$(aws ec2 describe-import-snapshot-tasks --import-task-ids ${snapshotTask} --query 'ImportSnapshotTasks[0].SnapshotTaskDetail.Status' --output text)" == "active" ]]
+do
+    aws ec2 describe-import-snapshot-tasks --import-task-ids $snapshotTask
+    err "import snapshot is still active."
+    sleep 60
+done
+
+snapshotId=$(aws ec2 describe-import-snapshot-tasks --import-task-ids ${snapshotTask} --query 'ImportSnapshotTasks[0].SnapshotTaskDetail.SnapshotId' --output text)
+
+err "Created snapshot: $snapshotId" 
+
+sleep 20
+
+DEVICE_MAPPINGS="[{\"DeviceName\": \"/dev/sda1\", \"Ebs\": {\"DeleteOnTermination\":true, \"SnapshotId\":\"${snapshotId}\", \"VolumeSize\":10, \"VolumeType\":\"gp2\"}}]"
+
+err $DEVICE_MAPPINGS
+
+ImageId=$(aws ec2 register-image --region $REGION --architecture=$ARCHITECTURE \
+	      --description="${NAME}-${MAJOR_RELEASE}.${MINOR_RELEASE} ($ARCH) for HVM Instances" \
+              --virtualization-type hvm --root-device-name '/dev/sda1' \
+              --name=${IMAGE_NAME} --ena-support --sriov-net-support simple \
+	      --block-device-mappings "${DEVICE_MAPPINGS}" \
+	      --output text)
+
+err "Produced Image ID $ImageId"
+echo "SNAPSHOT : ${snapshotId}, IMAGEID : ${ImageId}, NAME : ${IMAGE_NAME}" >> ${NAME}-${MAJOR_RELEASE}-${MINOR_RELEASE}.txt
+
+if [[ "$ARCHITECTURE" == "arm64" ]]
+then
+    INSTANCE_TYPE="c6g.large"
+else
+    INSTANCE_TYPE="c5n.large"
+fi
+
+err "aws ec2 run-instances --region $REGION --subnet-id $SUBNET_ID --image-id $ImageId --instance-type c5n.large --key-name "davdunc@amazon.com" --security-group-ids $SECURITY_GROUP_ID"
+aws ec2 run-instances --region $REGION --subnet-id $SUBNET_ID \
+    --image-id $ImageId --instance-type $INSTANCE_TYPE --key-name "previous" \
+    --security-group-ids $SECURITY_GROUP_ID $DRY_RUN && rm -f ./${IMAGE_NAME}.raw
+
+put_ssm_parameters
