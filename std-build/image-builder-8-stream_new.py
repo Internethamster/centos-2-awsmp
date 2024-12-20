9d0f3f8150f3284241f83a5aa2a48fcbf69e3258
David Duncan <davdunc@amazon.com>
2024-12-20 15:44:17 -0800

    refactor: move the click section to near the main function

--- std-build/image-builder-8-stream_new.py
+++ std-build/image-builder-8-stream_new.py
@@ -19,152 +19,7 @@ docstring = """
     the config file is found by default in the home directory of the user at /home/<username>/.config/centos_build_image.toml
 """
 
-@click.command()
-@click.option('--release-version', default=None, help='Release Version of CentOS Stream')
-@click.option('--revision', default='0', help='incremented when building additional images on the same build date')
-@click.option('--config-file', default=f'{os.environ["HOME"]}/.config/centos_build_config.toml', help='Path to the config file')
-    
-
-def import_snapshot(s3_object_name: str, s3_bucket_region: str) -> str:
-    '''
-    This function uses the import_snapshot api from the ec2 client to import the snapshot we just created. 
-    The function loops to determine the status of the snapshot import task for 6 minutes max and fails or until the snapshot import completes.
-    '''
-    config = load_config()
-    s3_bucket = config['marketplace']['s3_bucket']
-    s3_bucket_region = config['marketplace']['s3_bucket_region']
-    boto_config = Config(region_name=s3_bucket_region, profile_name=profile)
-    ec2 = boto3.client('ec2', config=boto_config)
-    response = ec2.import_snapshot(
-        Description=f'{s3_object_name} to {s3_bucket}',
-        DiskContainer={
-            'Description': f'{s3_object_name} HVM',
-            'Format': 'qcow2',
-            'UserBucket': {
-                'S3Bucket': s3_bucket,
-                'S3Key': f'{s3_bucket_prefix}{s3_object_name}'
-            },
-            'DeviceName': '/dev/sda1'
-        },
-        RoleName='centos-stream-import-role'
-    )
-    snapshot_task_id = response['SnapshotTaskId']
-    # Loop to determine the status of the snapshot import task for 6 minutes max and fails or until the snapshot import completes.
-    # The loop will sleep for 10 seconds between each iteration and will fail after 6 minutes
-    # The loop will exit if the snapshot import task fails
-    # The loop will return the snapshot id if the snapshot import task completes
-    # The loop will exit if the snapshot import task is not found
-    # The loop will exit if the snapshot import task is not in progress
-    # The loop will exit if the snapshot import task is not in the expected state
-    snapshot_status_complete = False
-    while not snapshot_status_complete:
-        response = ec2.describe_import_snapshot_tasks(
-            ImportTaskIds=[
-                snapshot_task_id,
-            ]
-        )
-        snapshot_status = response['ImportSnapshotTasks'][0]['SnapshotTaskDetail']['Status']
-        if snapshot_status == 'completed':
-            snapshot_status_complete = True
-            print(f'Snapshot import task {snapshot_task_id} completed')
-            snapshot_id = response['ImportSnapshotTasks'][0]['SnapshotTaskDetail']['SnapshotId']
-        elif snapshot_status == 'error':
-            snapshot_status_complete = False
-            print(f'Snapshot import task {snapshot_task_id} failed')
-            exit(1)
-        elif snapshot_status == 'cancelled':
-            snapshot_status_complete = False
-            print(f'Snapshot import task {snapshot_task_id} cancelled')
-            exit(1)
-        elif snapshot_status == 'cancelling':
-            snapshot_status_complete = False
-            print(f'Snapshot import task {snapshot_task_id} is failing')
-            time.sleep(5)
-        elif snapshot_status == 'pending':
-            print(f'Snapshot import task {snapshot_task_id} is pending')
-            time.sleep(10)
-        else:
-            snapshot_status_complete = False
-            print(f'Snapshot import task {snapshot_task_id} failed')
-            exit(1)
-    return snapshot_id
-def migrate_snapshot(snapshot_id: str, s3_object_name: str, s3_bucket_region: str) -> str:
-    """
-    This function uses the migrate_snapshot api from the ec2 client to migrate the snapshot we just created.
-    The function loops to determine the status of the snapshot migration task for 6 minutes max and fails or until the snapshot migration completes.
-    """
-    # copy the snapshot to the region from which it will be imported into marketplace. This is normally us-east-1 from the s3_bucket_region
-    # use the dynamodb table name created in the create_db.py file or found in the marketplace section of the centos_build_config.toml file
-    # use botocore to build a configuration for the boto3 client from the app.region and app.profile assignments in the toml file
-    boto_config = Config(region_name=s3_bucket_region, profile_name=profile)
-    ec2 = boto3.client('ec2', config=boto_config)
-    response = ec2.copy_snapshot(
-        Description=f'{s3_object_name} (HVM)',
-        DestinationRegion=region,
-        SourceRegion=s3_bucket_region,
-        SourceSnapshotId=snapshot_id
-    )
-    snapshot_id = response['SnapshotId']
-
-    dynamodb = boto3.resource('dynamodb', config=boto_config)
-    table = dynamodb.Table(config['marketplace']['dynamodb_table'])
-    #TODO: post the snapshot id for the product_id in the dynamodb table
-    table.update_item(
-        Key={
-            'centosid': product_id
-        },
-        UpdateExpression='SET snapshot_id = :val1',
-        ExpressionAttributesValues={
-            ':val1': snapshot_id
-        }
-    )
-    return snapshot_id
-
-def register_image(snapshot_id: str) -> str:
-    ''''
-    In this function, the snapshot id is used to register the image file as a new version of the AMI.
-    The function uses the register_image api from the ec2 client to register the image file as a new version of the AMI.
-    The function returns the ami id of the registered image.
-    '''
-    boto_config = Config(region_name=region, profile_name=profile)
-    ec2 = boto3.client('ec2', config=boto_config)
-    response = ec2.register_image(
-        Architecture=amzn_architecture,
-        BlockDeviceMappings=[
-            {
-                'DeviceName': '/dev/sda1',
-                'Ebs': {
-                    'DeleteOnTermination': True,
-                    'SnapshotId': snapshot_id,
-                    'VolumeSize': 8,
-                    'VolumeType': 'gp3'
-                }
-            }
-        ],
-        Description=f'CentOS Stream {release_version} ({amzn_architecture}) HVM {build_date}',
-        EnaSupport=True,
-        Imdsv2Support=True,
-        Name=f'{file_name}-{release_version}-{amzn_architecture}-{build_date}.{architecture}.{revision}',
-        RootDeviceName='/dev/sda1',
-        VirtualizationType='hvm',
-        SriovNetSupport='simple',
-        ImageLocation=f'{s3_bucket}/{s3_bucket_prefix}{file_name}-{release_version}-{build_date}.{architecture}.qcow2'
-    )
-    ami_id = response['ImageId']
-    boto_config = Config(region_name=region, profile_name=profile)
-    dynamodb = boto3.resource('dynamodb', config=boto_config)
-    table = dynamodb.Table(config['marketplace']['dynamodb_table'])
-    table.update_item(
-        Key={
-            'centosid': product_id
-        },
-        UpdateExpression='SET ami_id = :val1',
-        ExpressionAttributeValues={
-            ':val1': ami_id
-        }
-    )
-    return ami_id
-
+   
 
 def build_ami(release_version, config_file):
     """
@@ -247,5 +102,12 @@ def build_ami(release_version, config_file):
         )
         print(response)
 
+#DONE: include click for cli support
+@click.command()
+@click.option('--release-version', default=None, help='Release Version of CentOS Stream')
+@click.option('--revision', default='0', help='incremented when building additional images on the same build date')
+@click.option('--config-file', default=f'{os.environ["HOME"]}/.config/centos_build_config.toml', help='Path to the config file')
+ 
 if __name__ == '__main__':
+    
     build_ami(release_version=release_version, config_file=config_file)
